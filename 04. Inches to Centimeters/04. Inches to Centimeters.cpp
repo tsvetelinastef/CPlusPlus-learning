2cccfdc0a351f250694a83d188da1d75fbb7c739
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2022-08-22 20:31:43 +0300

    Add files via upload

---
+++ 04. Inches to Centimeters/04. Inches to Centimeters.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double number; 
+    cin >> number;
+
+    cout << number * 2.54;
+    
+    return 0;
+}
+
+/*  
+    double inches;
+    cin >> inches;
+    double centimeters = inches * 2.54;
+    cout << centimeters << endl;
+ */
